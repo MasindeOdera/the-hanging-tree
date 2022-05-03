c068b3636c381cf51083534898103d9719f06ae0
MasindeOdera <masindeodera@gmail.com>
2022-05-03 11:25:23 +0200

    New Deployment

---
+++ precache-manifest.2f47ae465c0b5254e7e241f3354b98f5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2428a68c6dbbc32e3d6",
+    "url": "/the-hanging-tree/css/about.5039eb92.css"
+  },
+  {
+    "revision": "bf8b578574a7aa617978",
+    "url": "/the-hanging-tree/css/app.88543e53.css"
+  },
+  {
+    "revision": "5ee0afb18ed93beae2eb",
+    "url": "/the-hanging-tree/css/chunk-043529c3.7301186f.css"
+  },
+  {
+    "revision": "20bf25e57993819653db",
+    "url": "/the-hanging-tree/css/chunk-1ff551fd.e9c95512.css"
+  },
+  {
+    "revision": "4399a07ac7b9966323e5",
+    "url": "/the-hanging-tree/css/chunk-2d0883f4.7541a77f.css"
+  },
+  {
+    "revision": "bdb8dfd9baf6bfde3c9a",
+    "url": "/the-hanging-tree/css/chunk-3dd4f9b0.aec1ba84.css"
+  },
+  {
+    "revision": "873aea2594e601ed5c7fb79082c185d0",
+    "url": "/the-hanging-tree/img/profile.873aea25.png"
+  },
+  {
+    "revision": "aa8c5213a2b545ba2dcf03c780b6b173",
+    "url": "/the-hanging-tree/index.html"
+  },
+  {
+    "revision": "e2428a68c6dbbc32e3d6",
+    "url": "/the-hanging-tree/js/about.defe6a67.js"
+  },
+  {
+    "revision": "bf8b578574a7aa617978",
+    "url": "/the-hanging-tree/js/app.89a8472c.js"
+  },
+  {
+    "revision": "5ee0afb18ed93beae2eb",
+    "url": "/the-hanging-tree/js/chunk-043529c3.96da8882.js"
+  },
+  {
+    "revision": "20bf25e57993819653db",
+    "url": "/the-hanging-tree/js/chunk-1ff551fd.9fa53964.js"
+  },
+  {
+    "revision": "4399a07ac7b9966323e5",
+    "url": "/the-hanging-tree/js/chunk-2d0883f4.5895802e.js"
+  },
+  {
+    "revision": "bdb8dfd9baf6bfde3c9a",
+    "url": "/the-hanging-tree/js/chunk-3dd4f9b0.eb31cda8.js"
+  },
+  {
+    "revision": "a22fef858f5a35547251",
+    "url": "/the-hanging-tree/js/chunk-vendors.41b40cc0.js"
+  },
+  {
+    "revision": "cd6278085234f16cd0b7f6c415484e86",
+    "url": "/the-hanging-tree/manifest.json"
+  },
+  {
+    "revision": "79c1465c99b50dc682b564200edd636d",
+    "url": "/the-hanging-tree/media/James_Newton_Howard_Ft._Jennifer_Lawrence-The_Hanging_Tree-320k.79c1465c.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/the-hanging-tree/robots.txt"
+  }
+]);
